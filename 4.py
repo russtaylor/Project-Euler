8a368fec72e0620c59585750cdc31d55953d5f39
Russ Taylor <russ@russt.me>
2013-04-28 23:25:52 -0600

    Finishes solution for problem 4.

--- 4.py
+++ 4.py
@@ -10,11 +10,23 @@
 import array
 import itertools
 
+def integer_is_palindrome(integer):
+    return string_is_palindrome(str(integer))
+
+def string_is_palindrome(string):
+    if len(string) < 2:
+        return True
+    if string[0] != string[-1]:
+        return False
+    return string_is_palindrome(string[1:-1])
+
+palindrome = 0
+
 numberArray = range(100,1000)
 
 for pair in itertools.combinations_with_replacement(numberArray, 2):
-    print pair
-    
+    product = pair[0] * pair[1]
+    if integer_is_palindrome(product) and product > palindrome:
+        palindrome = product
 
-def is_palindrome(integer):
-    string = str(integer)
+print palindrome
