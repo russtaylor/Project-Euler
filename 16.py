c3d22e361f00f4af2b4831be4603067afd9b513e
Russ Taylor <russ@russt.me>
2013-05-01 20:49:40 -0600

    Skipped ahead to problem 16, finished it.

---
+++ 16.py
@@ -0,0 +1,19 @@
+# Calculates the sum of the digits of 2^1000
+
+# Problem set from Project Euler
+# http://projecteuler.net
+# Problem 16
+
+# @author Russ Taylor <russ@russt.me>
+# @version 2013-05-01
+
+import math
+
+number = 2**1000
+numString = str(number)
+
+sum = 0
+for digit in numString:
+    sum += int(digit)
+
+print sum
