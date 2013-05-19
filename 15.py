316973c8d8506503c5f09c1214654e3a72c218b0
Russ Taylor <russ@russt.me>
2013-05-18 22:28:25 -0700

    Starting different strategy for 15.

--- 15.py
+++ 15.py
@@ -9,29 +9,8 @@ Problem 15
 @version 2013-05-05
 """
 
-# import time
-# import math
-# from itertools import repeat
-
-# # Love the recursive functions
-# def advance(array, index = None):
-#     if index == None:
-#         index = -1
-#     array[index] = (array[index] + 1) % 2
-#     if array[index] == 0 and math.fabs(index) < len(array):
-#         advance(array, index - 1)
-
-# numArray = list(repeat(0,40))
-
-# count = 0
-# for i in range(1,2**len(numArray)):
-#     advance(numArray)
-#     if sum(numArray) == 20:
-#         count += 1
-
-# print count
-
-sum = 0
-for i in range(1,20):
-    sum += 2*i
+grid = [[1]]
+max = 20
+for i in range(1,max):
+    
     print sum
