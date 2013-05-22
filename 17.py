cad17ff64ee5cbff314f65c54e0497b560eac24c
Russ Taylor <russ@russt.me>
2013-05-21 23:03:08 -0700

    Starting on problem 17.

---
+++ 17.py
@@ -0,0 +1,25 @@
+"""
+Calculates the number of letters used in writing all the numbers from 1-1000 in words.
+
+Problem set from Project Euler
+http://projecteuler.net
+Problem 17
+
+@author Russ Taylor <russ@russt.me>
+@version 2013-05-21
+"""
+
+numberNames = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve"
+}
