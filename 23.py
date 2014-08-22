9b53d65902088ad973fa5b7555fd9fd64ee55100
Russ Taylor <russ@russt.me>
2014-08-22 09:54:38 -0700

    Getting started on problem 23.

---
+++ 23.py
@@ -0,0 +1,11 @@
+"""
+Calculates the sum of all positive integers which can't be written as the sum of
+two abundant numbers.
+
+Problem set from Project Euler
+http://projecteuler.net
+Problem 23
+
+@author Russ Taylor <russ@russt.me>
+@version 2014-08-22
+"""
