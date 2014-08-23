6d36f5a819cb0a8c99226c048fa1f0be6f6ff0b6
Russ Taylor <russ@russt.me>
2014-08-23 16:45:00 -0700

    Making some progress, but still have to figure out the last bits.

--- 23.py
+++ 23.py
@@ -9,3 +9,32 @@ Problem 23
 @author Russ Taylor <russ@russt.me>
 @version 2014-08-22
 """
+
+import math
+
+from enum import Enum
+Abundance = Enum("Abundance", "deficient perfect abundant")
+
+def calculateDivisors(integer):
+  divisors = []
+  for i in range(1, math.floor(integer / 2 + 1)):
+    if integer % i == 0:
+      divisors.append(i)
+  return divisors
+
+def calculateAbundance(integer):
+  divisorSum = sum(calculateDivisors(integer))
+  if divisorSum > integer:
+    return Abundance.abundant
+  elif divisorSum < integer:
+    return Abundance.deficient
+  return Abundance.perfect
+
+def getAbundantBelow(integer):
+  abundantNumbers = []
+  for i in range(1, integer + 1):
+    if calculateAbundance(i) == Abundance.abundant:
+      abundantNumbers.append(i)
+  return abundantNumbers
+
+print(getAbundantBelow(28123))
